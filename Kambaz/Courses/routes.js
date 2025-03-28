b972055230f66abce7481f2c130c7d2b0f6aac86
kathyxiao <xiao.zixua@northeastern.edu>
2025-03-28 18:14:11 -0400

    v 6.4.1.4

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,20 +1,27 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = dao.findAllCourses();
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
     res.send(courses);
   });
-  app.delete("/api/courses/:courseId", (req, res) => {
+  app.post("/api/courses", async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    const course = await dao.createCourse(req.body);
+    await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    res.json(course);
+  });
+  app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
-    const status = dao.deleteCourse(courseId);
+    const status = await dao.deleteCourse(courseId);
     res.send(status);
   });
-  app.put("/api/courses/:courseId", (req, res) => {
+  app.put("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
-    const status = dao.updateCourse(courseId, courseUpdates);
+    const status = await dao.updateCourse(courseId, courseUpdates);
     res.send(status);
   });
   app.get("/api/courses/:courseId/modules", (req, res) => {
